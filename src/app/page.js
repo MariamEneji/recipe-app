02bd9ce0c2741bf7ab45e0efee3c1bb19bcbf0d3
Mariam Eneji <enejimariam1997@gmail.com>
2024-06-15 10:08:12 +0100

    fourth commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex gap-10 flex-col justify-center items-center h-screen bg-slate-400">
-      <h1 className="text-2xl lg:text-4xl ">Let's Make That Meal</h1>
+    <div className="flex gap-10 flex-col justify-between items-center h-screen bg-slate-400">
+      <h1 className="text-xl lg:text-4xl ">Let's Make That Meal</h1>
       <Link href={'recipes'} className='border p-4 border-black hover:bg-black hover:text-slate-400'>Explore Recipes Now</Link>
      
     </div>
